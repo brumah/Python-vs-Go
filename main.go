9e105d57824db8239ea4a12409cc3c1f445d1df0
brunerMatthew <bruner.matthewm@gmail.com>
2023-10-22 16:33:52 -0600

    created Go server

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+	"time"
+
+	"github.com/brunerMatthew/Python-vs-Go/queries"
+)
+
+func main() {
+	http.HandleFunc("/queries", queryHandler)
+	http.HandleFunc("/", rootHandler)
+
+	port := ":8080"
+	fmt.Printf("Server is listening on %s...\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("index.html")
+	initialText := "Hello"
+	tmpl.Execute(w, initialText)
+}
+
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	query := r.URL.Query()
+	id := query.Get("id")
+	var text string
+
+	switch id {
+	case "1":
+		text = queries.BlueButton()
+		fmt.Println(time.Since(start))
+	case "2":
+		text = queries.GreenButton()
+		fmt.Println(time.Since(start))
+	case "3":
+		text = queries.ClearButton()
+		fmt.Println(time.Since(start))
+	}
+
+	tmpl, _ := template.New("t").Parse(text)
+	tmpl.Execute(w, nil)
+}
---
+++ queries/queries.go
@@ -0,0 +1,127 @@
+package queries
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func BlueButton() string {
+	connectionString := "user=matthewbruner host=localhost port=5432 dbname=matthewbruner sslmode=disable"
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	query := "SELECT username, date_created, follower_count, post_count FROM user_data;"
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	swapHTML := `<table class='table-auto w-full border border-collapse text-center'>
+	<thead>
+	<tr>
+	<th class='px-4 py-2 border border-gray-900'>Username</th>
+	<th class='px-4 py-2 border border-gray-900'>Account Creation Date</th>
+	<th class='px-4 py-2 border border-gray-900'>Follower Count</th>
+	<th class='px-4 py-2 border border-gray-900'>Post Count</th>
+	</tr>
+	</thead>
+	<tbody>`
+
+	count := 0
+	for rows.Next() && count <= 4 {
+		var column1 string
+		var column2 time.Time
+		var column3, column4 int
+		if err := rows.Scan(&column1, &column2, &column3, &column4); err != nil {
+			log.Fatal(err)
+		}
+		swapHTML = swapHTML + fmt.Sprintf(`
+		<tr>
+		<td class='px-4 py-2 border border-gray-900'>%s</td>
+		<td class='px-4 py-2 border border-gray-900'>%s</td>
+		<td class='px-4 py-2 border border-gray-900'>%v</td>
+		<td class='px-4 py-2 border border-gray-900'>%v</td>
+		</tr>`,
+			column1, column2.Format("2006-01-02"), column3, column4)
+		count++
+	}
+	swapHTML = swapHTML + "</tbody></table>"
+	return swapHTML
+}
+
+func GreenButton() string {
+	connectionString := "user=matthewbruner host=localhost port=5432 dbname=matthewbruner sslmode=disable"
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	query := "SELECT username, date_created, follower_count, post_count FROM user_data WHERE follower_count < 10000;"
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	swapHTML := `<table class='table-auto w-full border border-collapse text-center'>
+	<thead>
+	<tr>
+	<th class='px-4 py-2 border border-gray-900'>Username</th>
+	<th class='px-4 py-2 border border-gray-900'>Account Creation Date</th>
+	<th class='px-4 py-2 border border-gray-900'>Follower Count</th>
+	<th class='px-4 py-2 border border-gray-900'>Post Count</th>
+	</tr>
+	</thead>
+	<tbody>`
+
+	count := 0
+	for rows.Next() && count <= 4 {
+		var column1 string
+		var column2 time.Time
+		var column3, column4 int
+		if err := rows.Scan(&column1, &column2, &column3, &column4); err != nil {
+			log.Fatal(err)
+		}
+		swapHTML = swapHTML + fmt.Sprintf(`
+		<tr>
+		<td class='px-4 py-2 border border-gray-900'>%s</td>
+		<td class='px-4 py-2 border border-gray-900'>%s</td>
+		<td class='px-4 py-2 border border-gray-900'>%v</td>
+		<td class='px-4 py-2 border border-gray-900'>%v</td>
+		</tr>`,
+			column1, column2.Format("2006-01-02"), column3, column4)
+		count++
+	}
+	swapHTML = swapHTML + "</tbody></table>"
+	return swapHTML
+}
+
+func ClearButton() string {
+	return `
+	<table class="table-auto w-full border border-collapse text-center" id="target">
+		<thead>
+			<tr>
+				<th class="px-4 py-2 border border-gray-900">Username</th>
+				<th class="px-4 py-2 border border-gray-900">Account Creation Date</th>
+				<th class="px-4 py-2 border border-gray-900">Follower Count</th>
+				<th class="px-4 py-2 border border-gray-900">Post Count</th>
+			</tr>
+		</thead>
+	</table>
+	`
+}
